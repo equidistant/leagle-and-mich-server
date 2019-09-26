7bf0b93c1448f395fdc8c07803673ecad4e0484b
equidistant <mich.random@pm.me>
2019-09-26 16:06:48 +0200

    images

--- src/image-size.js
+++ src/image-size.js
@@ -8,11 +8,11 @@ const readDir = promisify(fs.readdir)
 const sizeOf = promisify(imageSize)
 const router = express.Router()
 
-router.get('/:travelName', async (req, res, next) => {
-  const galleryPath = path.join(__dirname, `public/travels/${req.params.travelName}`)
+router.get('/:folderName/:subfolderName', async (req, res, next) => {
+  const galleryPath = path.join(__dirname, `public/${req.params.folderName}/${req.params.subfolderName}`)
   const images = await readDir(galleryPath)
   const ratios = []
-  for (let i = 1; i < images.length; i++) {
+  for (let i = 1; i <= images.length; i++) {
     const { width, height } = await sizeOf(`${galleryPath}/${i}.jpg`)
     ratios.push(width / height)
   }
