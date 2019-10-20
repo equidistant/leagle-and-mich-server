b3903fb62780314015cf25ad8fbd17106b592fb9
equidistant <mich.random@pm.me>
2019-10-20 16:08:57 +0200

    multiple galleries added

--- src/image-size.js
+++ src/image-size.js
@@ -8,12 +8,12 @@ const readDir = promisify(fs.readdir)
 const sizeOf = promisify(imageSize)
 const router = express.Router()
 
-router.get('/:folderName/:subfolderName', async (req, res, next) => {
-  const galleryPath = path.join(__dirname, `public/${req.params.folderName}/${req.params.subfolderName}`)
+router.get('/:folderName', async (req, res, next) => {
+  const galleryPath = path.join(__dirname, `public/${req.params.folderName}`)
   const images = await readDir(galleryPath)
   const ratios = []
-  for (let i = 1; i <= images.length; i++) {
-    const { width, height } = await sizeOf(`${galleryPath}/${i}.jpg`)
+  for (let i = 0; i < images.length; i++) {
+    const { width, height } = await sizeOf(`${galleryPath}/img${i}.jpg`)
     ratios.push(width / height)
   }
   res.setHeader('Access-Control-Allow-Origin', '*')
